14d573bbe5ad2073f01e3eb1f11812b5235b6ae9
Vinitj088 <vinitjain088@gmail.com>
2025-04-15 14:36:01 +0530

    Updated search UI

--- app/component/MobileSearchUI.tsx
+++ app/component/MobileSearchUI.tsx
@@ -258,7 +258,7 @@ const MobileSearchUI: React.FC<MobileSearchUIProps> = ({
               >
                 <div className="flex items-center justify-center gap-1">
                   <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M4 4L12 12M12 12L20 4M12 12L4 20M12 12L20 20" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
+                    <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                   </svg>
                   <span>SEARCH</span>
                 </div>
